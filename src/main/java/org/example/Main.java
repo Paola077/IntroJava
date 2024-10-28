a3088b1b34c2afcf6f926607f002ab3bef0bfad6
paolaperdomo <paolaperdomo079@gmail.com>
2024-10-28 10:52:34 +0100

    (feat): Numeros enteros y decimales añadidos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,29 @@ public class Main {
         System.out.println("Hello World");
 
         // escribir un boolean, asignarlo y mostrarlo por consola
-        boolean  isActive = true;
-
+        boolean isActive = true;
         System.out.println(isActive);
+
+        //numeros enteros
+
+        byte varByte = 7;
+        System.out.println(varByte);
+
+        short varShort = 27;
+        System.out.println(varShort);
+
+        int varInt = 75857;
+        System.out.println(varInt);
+
+        long varLong = 786504598;
+        System.out.println(varLong);
+
+        // float y decimal
+
+        float varFloat = 35;
+        System.out.println(varFloat);
+
+        double varDouble = 19.00;
+        System.out.println(varDouble);
     }
 }
