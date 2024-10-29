0cb6921df4c1454dca5eed116f0600080f74ec4a
paolaperdomo <paolaperdomo079@gmail.com>
2024-10-29 16:21:40 +0100

    refactor: Separando funcion printMenu del método Main de bookManager

--- src/main/java/org/example/bookManager/Main.java
+++ src/main/java/org/example/bookManager/Main.java
@@ -1,42 +1,9 @@
 package org.example.bookManager;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String [] menu = {
-              "1. Añadir libro",
-              "2. Ver todos los libros",
-              "3. Eliminar libro",
-                "4. Cambiar repositorio",
-              "5. Salir",
-        };
-
-        byte option = 0;
-
-        while(option != 5) {
-
-            for (String item : menu) {
-                System.out.println(item);
-            }
-
-            System.out.println("Selecciona una opción");
-
-            String input = scanner.nextLine(); // TODO comprobar que sea un numero "3"
-
-            try {
-                option = Byte.parseByte(input); // 1
-            } catch (Exception ex) {
-                System.out.println("Esto no es un número");
-                continue;
-            }
-
-            if(option <1 || option > 6) {
-                System.out.println("Número fuera de rango");
-                continue;
-            }
-            System.out.println("Ha elegido: " + menu[option - 1]);
-        }
+        Application app = new Application();
+        app.printMenu();
     }
+
 }
